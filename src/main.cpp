37c2335d901eee5eafb093bdb19c66a871d1561e
ggguilhermeee <goncalves-guilherme@outlook.com>
2022-04-06 22:55:26 +0100

    Setup opengl app

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <GL/gl.h>
 
 using namespace std;
 
+void init(GLFWwindow* window) { }
+
+void display(GLFWwindow* window, double currentTime) {
+    glClearColor(1.0, (int)currentTime % 2, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
 int main()
 {
-    cout << "Vomiting Words " << endl;
+    if (!glfwInit()) { exit(EXIT_FAILURE); }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    GLFWwindow* window = glfwCreateWindow(600, 600, "RenderStuff", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSwapInterval(1);
+    init(window);
+    while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+
+    cout << "Vomiting Words " << GLFW_JOYSTICK_6 << endl;
 }
