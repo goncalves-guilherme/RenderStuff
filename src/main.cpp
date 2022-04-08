1aa179b763ddfec649f6b5ad5e38fa0e8b79f84e
ggguilhermeee <goncalves-guilherme@outlook.com>
2022-04-08 23:33:45 +0100

    Render triangle using vbo and vfo

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,17 @@
 
 using namespace std;
 
-#define numVAOs 1
 GLuint renderingProgram;
-GLuint vao[numVAOs];
+unsigned int VBO, VAO;
 
 GLuint createShaderProgram()
 {
-    const char *vshaderSource =
-		"#version 430 \n"
-		"void main(void) \n"
-		"{ if(gl_VertexID == 0) gl_Position = vec4(0.25, -0.25, 0.0, 1.0); \n"
-        "else if(gl_VertexID == 1) gl_Position = vec4(-0.25, -0.25, 0.0, 1.0); \n"
-        "else gl_Position = vec4(0.25, 0.25, 0.0, 1.0);}";
+    const char *vshaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}";
 
     const char *fshaderSource =
 		"#version 430 \n"
@@ -23,46 +22,106 @@ GLuint createShaderProgram()
         "void main(void) \n"
 		"{ color = vec4(0.0, 1.0, 0.0, 1.0); }";
 
+    // Creates Vertex Shader
     GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-
     glShaderSource(vShader, 1, &vshaderSource, NULL);
-    glShaderSource(fShader, 1, &fshaderSource, NULL);
-
     glCompileShader(vShader);
+    
+    // Error Checking
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Creates Vertex Shader
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fShader, 1, &fshaderSource, NULL);
     glCompileShader(fShader);
 
+    // Error Checking
+    glGetShaderiv(fShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Link both shaders into a called program
     GLuint vfProgram = glCreateProgram();
     glAttachShader(vfProgram, vShader);
     glAttachShader(vfProgram, fShader);
     glLinkProgram(vfProgram);
 
+    // Check for linking errors
+    glGetProgramiv(vfProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(vfProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Opengl suggest to clean fragment and vertext shader when linked to a program
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
+
     return vfProgram;
 }
 
-void freeMemory()
+void addVerticeArrayBuffers()
 {
-    //glDeleteShader(vShader);
-    //glDeleteShader(fShader);  
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // left  
+         0.5f, -0.5f, 0.0f, // right 
+         0.0f,  0.5f, 0.0f  // top   
+    }; 
 
+    // Generate vertice array buffers. VAO & VBO
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+}
+
+void freeResources()
+{
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(renderingProgram);
 }
 
 void init(GLFWwindow* window)
 {
     renderingProgram = createShaderProgram();
-    glGenVertexArrays(numVAOs, vao);
-    glBindVertexArray(vao[0]);
 }
 
-void display(GLFWwindow* window, double currentTime) 
-{
-    glClearColor(1.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glUseProgram(renderingProgram);
-    glPointSize(10);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
-}
+void messageLogCallback(
+    GLenum source,
+    GLenum type,
+    GLuint id,
+    GLenum severity,
+    GLsizei length,
+    const GLchar *message,
+    const void *userParam)
+    {
+        std::cout << "OpenGL Error: " << message << std::endl;
+    }
 
 int main()
 {
@@ -71,7 +130,10 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "RenderStuff", NULL, NULL);
+    const unsigned int SCR_WIDTH = 800;
+    const unsigned int SCR_HEIGHT = 600;
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "RenderStuff", NULL, NULL);
 
     if (window == NULL)
     {
@@ -88,15 +150,34 @@ int main()
         return -1;
     }
 
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
     glfwSwapInterval(1);
+
     init(window);
-    while (!glfwWindowShouldClose(window)) {
-		display(window, glfwGetTime());
+
+    // Set Debug Handler
+    glEnable(GL_DEBUG_OUTPUT);
+    glDebugMessageCallback(&messageLogCallback, 0);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+    addVerticeArrayBuffers();
+
+    while (!glfwWindowShouldClose(window)) 
+    {
+		
+        glClearColor(1.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(renderingProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
     }
+
+    freeResources();
 
     glfwDestroyWindow(window);
     glfwTerminate();
